ea0ba8f443458b34ad3fa0e3a8d0edfe04531820
Tim Ambrose <luminoscity@gmail.com>
2018-05-29 04:20:59 -0700

    First running version of VFF

--- Common/getObstacleField.m
+++ Common/getObstacleField.m
@@ -1,8 +1,8 @@
-function [oddsGrid, occGrid] = getObstacleField(map, C, xMax, yMax, robot)
+function occGrid = getObstacleField(map, C, xMax, yMax, robot)
   global rangeMax;
 
   occGrid = zeros(C.R, C.C); %initialize as empty
-  oddsGrid = ones(C.R, C.C);
+  %oddsGrid = ones(C.R, C.C);
   rangeMax = C.rangeMax;
 
   Tl = se2([robot.x robot.y robot.theta]);
@@ -11,8 +11,8 @@ function [oddsGrid, occGrid] = getObstacleField(map, C, xMax, yMax, robot)
     angle = p(i,1); range = p(i,2);
     occGrid = updateLaserBeamBitmap(occGrid, angle, range, Tl, C.R, C.C, ...
       xMax, yMax);
-    oddsGrid = UpdateLaserBeamGrid(oddsGrid, angle, range, Tl, C.R, C.C, ...
-      xMax, yMax);
+    %oddsGrid = UpdateLaserBeamGrid(oddsGrid, angle, range, Tl, C.R, C.C, ...
+    %  xMax, yMax);
   end
 
 end
--- HW4/ObstacleField.m
+++ HW4/ObstacleField.m
@@ -13,12 +13,17 @@ Gmax = 60 * pi/180;
 L = 3;
 X = 1;
 Y = 2;
+goalX = 60;
+goalY = 80;
 N = 10;
 Vm = 4;
+span = pi;
+C = 500; R = 500;
+xMax = 100; yMax = 100;
 
 initAlloc = 1000;
 
-C = struct('L', L,...     %wheelbase [m]
+CC = struct('L', L,...     %wheelbase [m]
   'Rw', 0.5,...           %radius [m]
   'Vmax', Vm,...          %v max     [m/s]
   'Gmax', Gmax,...        %gamma max [radians]
@@ -29,55 +34,44 @@ C = struct('L', L,...     %wheelbase [m]
   'N', N,...              %crop rows
   'RL', 20, ...           %
   'HUGE', 10^9,...        %
-  'R', 500,...            %rows
-  'C', 500,...            %columns
-  'Fcr', 1.0,...          %repelling force constant
-  'Fct', 1.0,...          %attracting force constant
+  'R', R,...            %rows
+  'C', C,...            %columns
+  'obstWindow', 40, ...   %window to calculte repulsive forces (meters)
+  'Fcr', 0.0,...         %repelling force constant
+  'Fct', 10.0,...         %attracting force constant
   'd0', 2.0,...           %threshold distance for repulsion
   'ptsPerMeter', 2,...    %
   'posEpsilon', 0.2,...   %position requirement
   'rangeMax' , 200, ...   %max range of laser
-  'angleSpan', pi, ...    %angle span of laser sweep
-  'angleStep', pi/360, ...   %step of laser sweep
+  'angleSpan', span, ...  %angle span of laser sweep
+  'angleStep', span/360, ... %step of laser sweep
   'Rmin', L / tan(Gmax), ... %Min turning radius
   'MULT', 5, ...          %multiplier
-  'redrawT',0.2 / Vm,...  %# of DT to redraw robot for pursuit controller
+  'redrawT',0.05 / Vm,...  %# of DT to redraw robot for pursuit controller
   'aniPause', 0.001 ...   %animation pause
   );
 
-map=zeros(C.R, C.C);
+map=zeros(R, C);
 Xsw=20; Ysw = 30;
 Xne=Xsw + 40; Yne= Ysw + 20;
-xMax = 100; yMax = 100;
-[Isw, Jsw] = XYtoIJ(Xsw, Ysw, xMax, yMax, C.R, C.C);
-[Ine, Jne] = XYtoIJ(Xne, Yne, xMax, yMax, C.R, C.C);
+[Isw, Jsw] = XYtoIJ(Xsw, Ysw, xMax, yMax, R, C);
+[Ine, Jne] = XYtoIJ(Xne, Yne, xMax, yMax, R, C);
 map(Ine:Isw, Jsw:Jne) = 1;
 
-robot = DrawableRobot(10, 10, pi/2, C.Rw, C.L, C.Gmax, C.Vmax, C.Ld);
+robot = DrawableRobot(80, 40, pi/2, CC.Rw, CC.L, CC.Gmax, CC.Vmax, CC.Ld);
 frame = [0, xMax, 0, yMax];
 robot.DrawRobot(frame, true);
 
+
 hold on
-[~, occGrid] = getObstacleField(map, C, xMax, yMax, robot);
 %{
-[ii,jj] = find(occGrid);
-[xx, yy] = IJtoXY(ii, jj, xMax, yMax, C.R, C.C);
-plot(xx, yy, 'b.')
-%
 figure
 imagesc(occGrid)
 colorbar
 %}
 
-Fr = zeros(C.C, C.R, 2);   % (x, y, {x, y})  third dim is vector xy components
-Ft = zeros(C.C, C.R, 2);   %attracting forces
-for y = 1:C.R
-  for x = 1:C.C
-    d = sqrt((x - robot.x)^2 + (y - robot.y)^2);
-    %[(x - robot.x);(y - robot.y)];
-    Fr(x, y, :) = Ft(x, y, :) ./ C.Fct .* C.Fcr .* occGrid(y, x) ./ (d^2);
-  end
-end
+% (x, y, [x y])  third dimension is vector xy components
+FrGrid = zeros(2 * CC.obstWindow + 1, 2 * CC.obstWindow + 1, 2);
 
 pathPoint = 'b.';
 tauG = 0.15;
@@ -85,26 +79,56 @@ tauV = 0.5;
 s = 0;
 skidDR = 0;
 skidDF = 0;
-vd = C.Vmax;
+vd = CC.Vmax;
+obstPlot = plot(0,0,'k.');
+occGrid = getObstacleField(map, CC, xMax, yMax, robot);
+fcr = CC.Fcr;
 
-for t = 0:C.DT:(C.T - C.DT)
-  redraw = mod(t, C.redrawT) == 0;
+for t = 0:CC.DT:(CC.T - CC.DT)
+  redraw = mod(t, CC.redrawT) == 0;
   if redraw
     prevPos = [robot.x, robot.y];
   end
-
-  if k > length(stepErr)
-    stepErr = [stepErr, zeros(1, length(stepErr))];
+  
+  
+  if redraw
+    occGrid = getObstacleField(map, CC, xMax, yMax, robot);
+    [ii,jj] = find(occGrid);
+    [xx, yy] = IJtoXY(ii, jj, xMax, yMax, R, C);
+    delete(obstPlot)
+    obstPlot = plot(xx, yy, 'b.');
   end
-  [gammaD, err, prev, errX, errY] = planner1.FirstFeasiblePoint(robot, prev);
-  delete(lookAhead)
-  lookAhead = plot(pathPoints(X, prev), pathPoints(Y, prev), 'k*');
-  stepErr(k) = err;
-  k = k + 1;
-
-  robot.MoveEulerAck_RedrawRobot(gammaD, vd, C, s, skidDR, skidDF, tauV, ...
+  
+  rx = robot.x;
+  ry = robot.y;
+  errX = goalX - rx;
+  errY = goalY - ry;
+  Ft = CC.Fct / sqrt(errX^2 + errY^2) .* [errX, errY];
+  
+  xL = round(max(1, rx - CC.obstWindow));
+  xR = round(min(xMax, rx + CC.obstWindow));
+  yD = round(max(1, ry - CC.obstWindow));
+  yU = round(min(yMax, ry + CC.obstWindow));
+  FrX = 0;
+  FrY = 0;
+  parfor x = xL:xR
+    J = round((x / xMax) * (C-1)) + 1;
+    for y = yD:yU
+      I = round(((yMax - y) / yMax) * (R-1)) + 1;
+      d = max(sqrt((x - rx)^2 + (y - ry)^2), 1e-6);
+      occ = occGrid(I, J) * fcr / (d^3);
+      FrX = FrX + occ * (x - rx);
+      FrY = FrY + occ * (y - ry);
+    end
+  end
+  Rx = FrX + Ft(X);
+  Ry = FrY + Ft(Y);
+  gammaD = atan2(Ry, Rx);
+  %vd = sqrt(Rx^2 + Ry^2);
+  
+  robot.MoveEulerAck_RedrawRobot(gammaD, vd, CC, s, skidDR, skidDF, tauV, ...
     tauG, frame, prevPos, pathPoint, redraw);
-  if prev == planner1.nPoints && abs(errX) + abs(errY) < C.posEpsilon
+  if abs(errX) + abs(errY) < CC.posEpsilon
     break   % stop if navigating to last path point and position close enough
   end
 end
